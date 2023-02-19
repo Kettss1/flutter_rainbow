5c98daa888d944b996cd2e0a1eb7c86816264ba9
Kettss1 <ketlintais6@gmail.com>
2023-02-19 18:16:00 -0300

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_rainbow/rainbow.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,7 +13,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const Rainbow());
 
-    // Verify that our counter starts at 0.
+    // Verify that the text is "Hey there".
     expect(find.text('Hey there'), findsOneWidget);
   });
 }
